b8b430830de64c82c91e137426a772a0e2898bbc
Yousuf Nejati <yousufnejati@gmail.com>
2020-02-23 07:09:55 -0800

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -2,27 +2,43 @@
 #[macro_use]
 extern crate actix_web;
 
-use actix_web::{ middleware, web, App, HttpRequest, HttpServer, Result };
+use actix_web::{ middleware, web, App, HttpServer, Result };
 use serde::Serialize;
+use std::cell::Cell;
+// Tools needed to work with a usize that can be modified atomically and thefore threadsafe.
+use std::sync::atomic::{AtomicUsize, Ordering};
+// Tools to safely share and mutate things that are not atomic accross multiple threads.
+use std::sync::{Arc, Mutex};
+
+static SERVER_COUNTER: AtomicUsize = AtomicUsize::new(0);
 
 // Handlers
 // uses serde serialize
 #[derive(Serialize)]
 struct IndexResponse {
-  message: String,
+  server_id: usize,
+  request_count: usize,
+  messages: Vec<String>
+}
+
+// Each worker thread will get own instance of this struct.
+struct AppState {
+  server_id: usize,
+  request_count: Cell<usize>,
+  messages: Arc<Mutex<Vec<String>>>,
 }
 
 #[get("/")]
-fn index(req: HttpServer) -> Result<web::Json<IndexResponse>> {
-  let hello = req
-    .headers
-    .get("hello")
-    .and_then(|v| v.to_str().ok())
-    .unwrap_or_else(|| "world");
+fn index(state: web::Data<AppState>) -> Result<web::Json<IndexResponse>> {
+  let request_count = state.request_count.get() + 1;
+  state.request_count.set(request_count);
+  let ms = state.messages.lock().unwrap();
 
-    Ok(web::Json(IndexResponse {
-      message: hello.to_owned(),
-    }))
+  Ok(web::Json(IndexResponse {
+    server_id: state.server_id,
+    request_count,
+    messages: ms.clone()
+  }))
 }
 
 pub struct MessageApp {
@@ -35,11 +51,18 @@ impl MessageApp {
   }
 
   pub fn run(&self) -> std::io::Result<()> {
-    println!("Starting up http server at: localhost:{}", self.port);
+    // Shared messages vector among workers.
+    let messages = Arc::new(Mutex::new(vec![]));
+    println!("Starting http server: 127.0.0.1:{}", self.port);
     // Build out server.
     HttpServer::new(move || {
       App::new()
-        .middleware(middleware::Logger::default())
+        .data(AppState {
+          server_id: SERVER_COUNTER.fetch_add(1, Ordering::SeqCst),
+          request_count: Cell::new(0),
+          messages: messages.clone(),
+        })
+        .wrap(middleware::Logger::default())
         .service(index)
     })
     .bind(("127.0.0.1", self.port))?
